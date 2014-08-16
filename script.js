88eff61fa78166ae987f8ac649560a141fb98df5
Alexis López Espinoza <Alexis88@users.noreply.github.com>
2014-08-16 03:13:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var myForm = document.getElementsByTagName("form")[0],
-           myOutput = document.getElementById("output"),
-           ajax = function(form, ouput){
+             myOutput = document.getElementById("output"),
+             ajax = function(form, ouput){
                 var xhr = window.XMLHttpRequest ? 
                          new XMLHttpRequest() : 
                          new ActiveXObject("Microsoft.XMLHTTP") || 
