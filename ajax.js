0e889c5d50491ad6b29d76e76aafb97e4e31c262
Alexis López Espinoza <Alexis88@users.noreply.github.com>
2015-05-19 20:48:44 -0500

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,15 +1,15 @@
 var myForm = document.getElementsByTagName("form")[0],
 	myOutput = document.getElementById("output"),
 	ajax = function(form, output){
-	var xhr = window.XMLHttpRequest ? 
+		var xhr = window.XMLHttpRequest ? 
 	   		  new XMLHttpRequest() : 
 	          new ActiveXObject("Microsoft.XMLHTTP") || 
 	  		  new ActiveXObject("Msxml2.XMLHTTP"),
 	  	elements = form.elements,
-	    total = elements.length,
-	    url = form.action,
-	    data = new FormData(),
-	    cargando = document.getElementById("cargando"), img;
+	    	total = elements.length,
+			url = form.action,
+	    	data = new FormData(),
+	    	cargando = document.getElementById("cargando"), img;
 
 	    for (var i = 0; i < total; i++){
 	       	if (elements[i].type == "file"){
